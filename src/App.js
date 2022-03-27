7e4dc814df2fb484a5bee515b935f0dfcba425ba
Daniel Jurkiewicz <38923739+daniel-jurkiewicz@users.noreply.github.com>
2022-03-27 16:56:03 +0200

    Added table component with data fetching

--- src/App.js
+++ src/App.js
@@ -1,5 +1,55 @@
+import React, { useState, useEffect } from "react";
+import Table from "./Table";
+
 function App() {
-	return <></>;
+	const [cells, setCells] = useState([]);
+
+	const getData = () => {
+		fetch("https://api.nbp.pl/api/exchangerates/tables/A/?format=json")
+			.then((response) => {
+				if (response.ok) {
+					return response.json();
+				}
+				throw new Error("Response not OK");
+			})
+			.then((data) => {
+				const { rates } = data[0];
+				const cells = rates.map((rate) => {
+					return {
+						currency: rate.currency,
+						code: rate.code,
+						mid: rate.mid,
+					};
+				});
+				setCells(cells);
+			});
+	};
+
+	const columns = React.useMemo(
+		() => [
+			{
+				Header: "Symbol waluty",
+				accessor: "code",
+			},
+			{
+				Header: "Waluta",
+				accessor: "currency",
+			},
+			{
+				Header: "Kurs waluty",
+				accessor: "mid",
+			},
+		],
+		[],
+	);
+
+	useEffect(() => {
+		getData();
+	}, []);
+
+	const data = React.useMemo(() => cells, [cells]);
+
+	return <Table columns={columns} data={data} />;
 }
 
 export default App;
